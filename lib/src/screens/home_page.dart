44c6b1a2fbb82d80b8048925b73b61a9e3eb669a
Leo <leoaromero2015@gmail.com>
2021-01-31 10:27:43 -0300

    Actualizacion de usuarios

--- lib/src/screens/home_page.dart
+++ lib/src/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modulo1_fake_backend/models.dart';
 import 'package:recetas/src/components/my_drawer.dart';
+import 'package:recetas/src/components/recipe_widget.dart';
 import 'package:recetas/src/connection/server_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -31,46 +32,12 @@ class _HomePageState extends State<HomePage> {
               itemCount: list.length,
               itemBuilder: (context, index) {
                 Recipe recipe = list[index];
-                return Padding(
-                  padding: EdgeInsets.fromLTRB(15, 20, 15, 0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: Card(
-                      child: Container(
-                        height: 250,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: FileImage(recipe.photo),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        alignment: Alignment.bottomLeft,
-                        child: Container(
-                          color: Colors.black.withOpacity(0.35),
-                          child: ListTile(
-                            title: Text(
-                              recipe.name,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                                fontSize: 16,
-                              ),
-                            ),
-                            subtitle: Text(
-                              recipe.user.nickname,
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            trailing: IconButton(
-                              icon: Icon(Icons.favorite),
-                              onPressed: () {},
-                              iconSize: 32,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                return RecipeWidget(
+                  recipe: recipe,
+                  serverController: widget.serverController,
+                  onChange: () {
+                    setState(() {});
+                  },
                 );
               },
             );
